f0355ad122bc13278cd378dd17078566fea897bf
Kalys01 <kalysosmon96.g@gmail.com>
2021-08-30 23:04:32 +0600

    we save the selected topic in localstorage

--- src/store/modules/theme.js
+++ src/store/modules/theme.js
@@ -1,7 +1,12 @@
 export default {
   state: {
+    theme: {}
   },
   mutations: {
+      SET_THEME(state, theme) {
+        state.theme = theme;
+        localStorage.theme = theme;
+      }
   },
   actions: {
   },
