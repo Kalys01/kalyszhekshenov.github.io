1ff2464cd5a8c4de58c23b026596803ff62e074e
Kalys01 <kalysosmon96.g@gmail.com>
2021-09-20 21:27:21 +0600

    added new button hamburger menu and sideBar

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,49 @@
 <template>
   <div id="app">
-    <Nav-bar/>
-    <component :is="layout">
-      <router-view/>
-    </component>
+    <div class="relative">
+      <TheNavBar />
+      <component :is="layout">
+        <router-view />
+      </component>
+      <HamburgerMenu />
+      <div
+         id="btn"
+        class="absolute z-0 border-black top-0 -left-52 shadow w-52 h-full bg-black md:hidden dark:bg-gray-300"
+      ></div>
+    </div>
   </div>
 </template>
 
 <script>
-import NavBar from '@/components/NavBar'
-import { mapGetters } from 'vuex'
+import TheNavBar from "@/components/TheNavBar.vue";
+import HamburgerMenu from "@/components/HamburgerMenu.vue";
+import { mapGetters } from "vuex";
 
 export default {
   name: "App",
   components: {
-    NavBar
+    TheNavBar,
+    HamburgerMenu
   },
   beforeMount() {
     this.$store.dispatch("INIT_THEME");
   },
   computed: {
     layout() {
-      const layoutName = this.$route.meta.layout || 'MainLayout';
-      return () => import(`@/layouts/${layoutName}.vue`)
+      const layoutName = this.$route.meta.layout || "MainLayout";
+      return () => import(`@/layouts/${layoutName}.vue`);
     },
-    ...mapGetters({theme: "GET_THEME"})
+    ...mapGetters({ theme: "GET_THEME" })
   },
   watch: {
     theme(newTheme, oldTheme) {
       newTheme === "light"
-      ? document.querySelector("html").classList.remove('dark')
-      : document.querySelector("html").classList.add('dark');
-    },
-  },
+        ? document.querySelector("body").classList.remove("dark")
+        : document.querySelector("body").classList.add("dark");
+    }
+  }
 };
 </script>
 
 <style lang="scss">
-
 </style>
